79ddf452f817f163da8f6b52c807c3304a7037cf
Lucas <lucas.2020109662@unicap.br>
2023-09-22 10:41:30 -0300

    Jogo de Dado

--- Atividade 6/script.js
+++ Atividade 6/script.js
@@ -3,9 +3,11 @@ document.addEventListener("DOMContentLoaded", function (){
     const resultadoElemento = document.getElementById('resultado');
     const giroElemento = document.getElementById('giro');
     const inputElemento = document.getElementById('input');
+    const dado1 = document.getElementById('diceBox1');
+    const dado2 = document.getElementById('diceBox2');
 
     function girarDado(){
-        return Math.floor(Math.random() * 5) + 1;
+        return Math.floor(Math.random() * 6) + 1;
     }
 function playGame() {
     const diceRoll1 = girarDado();
@@ -15,19 +17,17 @@ function playGame() {
     const userNumber = parseInt(input);
 
     if (input === null) {
-        // User canceled input
         return;
     }
-
     if (userNumber === diceRoll1 || userNumber === diceRoll2) {
-        resultadoElemento.textContent = "You win!";
+        resultadoElemento.textContent = "Você ganhou!!";
     } else if (userNumber >= 1 && userNumber <= 6) {
-        resultadoElemento.textContent = "You lose!";
+        resultadoElemento.textContent = "Você perdeu!";
     } else {
-        resultadoElemento.textContent = "Invalid number";
+        resultadoElemento.textContent = "Número inválido, digite novamente";
     }
-
-    giroElemento.textContent = `Dice roll 1: ${diceRoll1}, Dice roll 2: ${diceRoll2}`;
+    dado1.textContent= "Dado 1: " +"\n " + diceRoll1;
+    dado2.textContent= "Dado 2: " +"\n "+ diceRoll2;
 }
 
 rodar.addEventListener("click", playGame);
